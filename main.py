4a6acd737a64918cb1ae071bc96d76daa989fdea
Ari k <75741608+ArikSquad@users.noreply.github.com>
2022-03-07 12:26:50 +0200

    Removed comments

--- main.py
+++ main.py
@@ -1,50 +1,30 @@
-# Imports
 import pyautogui
 import time
 
 
-# Variables
-name = str("AriSpammer")
-print("AriModules")
-print(f"Currently using the {name}")
-spam = str(input("Spamming > "))
-times = int(input("Times > "))
+spam = str(input("The text you want to spam? > "))
+times = int(input("How many times? > "))
 
-# Check if string is return.
 if spam is not None:
     print(f"Okay, so we are spamming ({spam})! Waiting 5 seconds")
-# If not then use your last copy.
 else:
-    print(f"Okay, so we are spamming your paste! Waiting 5 seconds")
+    print(f"Okay, so we are spamming your clipboard! Waiting 5 seconds")
 
-# Start sleeping
 time.sleep(5)
-print("Starting")
+print("Starting the spammer")
 
-# The variables to show how many times it's been ran.
 s = 0
-a = 1
 
-# loop
 for i in range(times):
-    # Make the times.
-    s = s + a
+    s = s + 1
 
-    # If string is returned then spam the string
     if spam is not None:
         pyautogui.typewrite(spam)
-
-    # If the string is not returned then paste
     else:
         pyautogui.hotkey('ctrl', 'v', interval=0.15)
-
-    # Send the message. Usually the 'enter' key.
     pyautogui.press("enter")
 
-    # Prints how many times it's been ran.
-    print(f"Did this {s} times")
+    print(f"Sent keystrokes {s} times")
 
-# Prints Successful after all.
-print("Successful!")
-# Exits the program safely.
+print(f"Spammer end. {s} times in total.")
 exit()
